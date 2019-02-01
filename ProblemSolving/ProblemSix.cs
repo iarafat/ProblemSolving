18d9d99842185483b3ccb47b5c594400df1f3a98
iarafat <miyasinarafat@gmail.com>
2019-02-01 13:53:32 -0800

    solved string reverse problem

---
+++ ProblemSolving/ProblemSix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//links: https://devskill.com/CodingProblems/ViewProblem/26
+
+namespace ProblemSolving
+{
+    class ProblemSix
+    {
+        static void Main(string[] args)
+        {
+            var loop = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < loop; i++)
+            {
+                var values = Console.ReadLine();
+                Console.WriteLine(Reverse(values.ToString()));
+            }
+
+        }
+        public static string Reverse(string s)
+        {
+            char[] chars = s.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+    }
+}
